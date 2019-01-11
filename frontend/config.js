0735b170661cd8fd14b03ef3b9009d21c1bf1584
Tom Wilson <tom@iwm.pw>
2019-01-11 14:08:47 +0000

    deployment

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://192.168.0.98:5555`;
+export const prodEndpoint = `https://three-amigos-yoga-prod.herokuapp.com`;
 export const perPage = 4;
